7c00b99d0142619dd1fed97b476323b4de7f50a6
ReVuZ <revathymr916@gmail.com>
2026-05-01 16:10:18 +0530

    step 2 chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    panic!("Hello, world!");
+use std::io::{self,Read};
+
+fn main(){
+    for b in io::stdin().bytes(){
+        let c:char = b.unwrap() as char;
+        println!("{}",c);
+    }
 }
